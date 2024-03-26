595a18ea6447704070e01760032b8ad4b9055147
Undiluted7027 <undiluted7027@protonmail.com>
2024-03-26 02:35:03 -0700

    Add waste model with users model, routes, and models

--- backend/index.js
+++ backend/index.js
@@ -5,12 +5,14 @@ const app = express();
 const userRoute = require('./routes/userRoutes');
 const transportationRoute = require('./routes/transportationRoutes');
 const foodRoute = require('./routes/foodRoutes');
+const wasteRoute = require('./routes/wasteRoutes');
 
 app.use(cors());
 app.use(express.json());
 app.use('/api', userRoute);
 app.use('/api/transportation', transportationRoute);
 app.use('/api/food', foodRoute);
+app.use('/api/waste', wasteRoute);
 
 app.listen(3000, () =>
     console.log(`Server is live`),
