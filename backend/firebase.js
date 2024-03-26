4d2b09ab8a1ce424f3465253f981c46d5dd0e364
Undiluted7027 <undiluted7027@protonmail.com>
2024-03-26 00:41:12 -0700

    Add users model with routes and controllers

--- backend/firebase.js
+++ backend/firebase.js
@@ -1,12 +1,12 @@
-const { initializeApp, cert } = require('firebase-admin/app')
-const { getFirestore } = require('firebase-admin/firestore')
+const { initializeApp, applicationDefault, cert } = require('firebase-admin/app');
+const { getFirestore, Timestamp, FieldValue, Filter } = require('firebase-admin/firestore');
 
-const serviceAccount = require('./creds.json')
+const serviceAccount = require('./creds.json');
 
 initializeApp({
-    credential: cert(serviceAccount)
-})
+  credential: cert(serviceAccount)
+});
 
-const db = getFirestore()
+const db = getFirestore();
 
-module.exports = { db }
+module.exports = db;
